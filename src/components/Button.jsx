9dfb80f3cc0a7e58c984180df37edb5657bfb0f3
ANIKETHPAWAR <Anikethpawar05@gmail.com>
2025-01-08 09:16:26 +0530

    Streak

---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+export const Button = ({
+    disabled,
+    children,
+    onClick
+})=> {
+return <span onClick={onClick} className={`px-32 py-text-white cursor-pointer ${disabled ? "bg-blue-200" :"bg-green-400"}`}>{children}</span>
+}
